22839094ab3ac04575df41e2cc4db1eab0815502
Alanakarine-17 <alanacantuaria@gmail.com>
2021-11-02 17:54:01 -0300

    home

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,11 @@
 import * as S from "./styles";
+import anuncio from "assets/img/espaço para anúncios.svg";
+
 
 const Main = () => {
     return (
         <S.Main>
-            <h1>ESPAÇO PARA IMAGENS</h1>
+            <img src={anuncio} alt="Espaço para anúncios"/>
         </S.Main>
     )
 }
